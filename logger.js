098ea84c44a07fd8b3aa5f58d56a9f295738234c
desi <f.desimone30@studenti.unisa.it>
2019-01-20 18:02:42 +0100

    add IFTT support

--- logger.js
+++ logger.js
@@ -9,15 +9,10 @@ async function init() {
     const conn = await amqp.connect('amqp://guest:guest@' + argv.ip + ':5672')
     const channel = await conn.createChannel()
     await channel.assertExchange('babycare/logger', 'direct', { durable: false })
-    const q  = await channel.assertQueue('', { exclusive: true })
+    const q = await channel.assertQueue('', { exclusive: true })
     argv._.forEach(async routing => {
         await channel.bindQueue(q.queue, 'babycare/logger', routing)
     });
-    await channel.consume(q.queue, function (msg) {
-        const d = new Date()
-        const now = d.getUTCFullYear() +"/"+ (d.getUTCMonth()+1) +"/"+ d.getUTCDate() + " " + d.getUTCHours() + ":" + d.getUTCMinutes() + ":" + d.getUTCSeconds()
-        console.log(" ["+now+"] %s: '%s'", msg.fields.routingKey, msg.content.toString());
-    }, { noAck: true });
-
+    await channel.consume(q.queue, (msg) => console.log(" [" + new Date().toISOString() + "] %s: '%s'", msg.fields.routingKey, msg.content.toString()), { noAck: true });
 }
 init()
--- smart_watch.js
+++ smart_watch.js
@@ -7,12 +7,8 @@ if (argv.h || argv._.length === 0) {
 }
 
 setInterval(() => {
-    let hearth = Math.random() * 40 + 50
-    let temp = Math.random() * 4 + 36
-    if (temp >= 37 || hearth <= 60) {
-        mqtt.send_message(argv._[0], { warning: true, hearth: hearth, temp: temp }, "babycare/health")
-    } else {
-        mqtt.send_message(argv._[0], { warning: false, hearth: hearth, temp: temp }, "babycare/health")
-    }
+    const hearth = Math.random() * 40 + 50
+    const temp = Math.random() * 4 + 36
+    mqtt.send_message(argv._[0], { warning: temp >= 37 || hearth <= 60, hearth: hearth, temp: temp }, "babycare/health")
 }, 5000);
 
